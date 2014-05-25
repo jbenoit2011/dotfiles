2694683764043ec25fa13b1acee89d5201df33f4
Julien BENOIT <jbenoit.jb@gmail.com>
2014-05-25 18:18:38 +0200

    Add bash conf

---
+++ bash/bash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo This script gonna add some aliases to your bashrc file
+sleep 2 
+
+# ls
+bashrcLine[0]="alias ll='ls -alF'"
+bashrcLine[1]="alias la='ls -A'"
+bashrcLine[2]="alias l='ls -CF'"
+# apt
+bashrcLine[3]="alias apt-get='sudo apt-get'"
+
+# Add to bashrc
+lineAdded=0
+for line in "${bashrcLine[@]}"
+do
+    grep "${line}" ~/.bashrc > /dev/null
+    if [ $? -ne 0 ]; then
+       echo "${line}" >> ~/.bashrc
+       echo "[OK] ${line} added to your bashrc."
+       ((lineAdded++))
+    else
+       echo "[FAIL] ${line} already exists in bashrc file."
+    fi 
+done
+
+# source bashrc
+echo
+echo "${lineAdded} line(s) added to bashrc"
+if [ $lineAdded -gt 0 ]; then
+    echo "Please run 'source ~/.bashrc' to enable aliases in your current shell"
+fi

050365073810558e27fefe59305f8422dc13c66d
Julien BENOIT <jbenoit.jb@gmail.com>
2014-05-25 20:10:30 +0200

    Update bashrc conf

--- bash/bash.sh
+++ bash/bash.sh
@@ -4,15 +4,17 @@ echo This script gonna add some aliases to your bashrc file
 sleep 2 
 
 # ls
-bashrcLine[0]="alias ll='ls -alF'"
-bashrcLine[1]="alias la='ls -A'"
-bashrcLine[2]="alias l='ls -CF'"
+bashrc[0]="alias ll='ls -alF'"
+bashrc[1]="alias la='ls -A'"
+bashrc[2]="alias l='ls -CF'"
 # apt
-bashrcLine[3]="alias apt-get='sudo apt-get'"
+bashrc[3]="alias apt-get='sudo apt-get'"
+# dpkg
+bashrc[4]="alias dpkgi='sudo dpkg -i'"
 
 # Add to bashrc
 lineAdded=0
-for line in "${bashrcLine[@]}"
+for line in "${bashrc[@]}"
 do
     grep "${line}" ~/.bashrc > /dev/null
     if [ $? -ne 0 ]; then
